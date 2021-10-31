6acd3fc6e9c8ad36a2e61dd77838ca3ed430e055
RalGoX1 <293346@viauc.dk>
2021-10-31 19:26:33 +0100

    Updated

--- DNP_Assignment2/Models/User.cs
+++ DNP_Assignment2/Models/User.cs
@@ -1,10 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace DNP_Assignment2.Models
 {
     public class User
     {
         public string Email { get; set; }
+        
         public string Password { get; set; }
-        public string FisrtName { get; set; }
+        
+        public string FirstName { get; set; }
+        
         public string LastName { get; set; }
     }
 }
